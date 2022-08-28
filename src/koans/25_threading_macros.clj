193644a7332ac7289a30f39fe8f3e35b9962e43c
Alexey Beshenov <abeshenov@pasteursoft.com>
2022-08-27 23:52:57 -0500

    Add solutions

--- src/koans/25_threading_macros.clj
+++ src/koans/25_threading_macros.clj
@@ -15,52 +15,52 @@
 
 (meditations
  "We can use thread first for more readable sequential operations"
- (= __
+ (= {:a 1}
     (-> {}
         (assoc :a 1)))
 
  "Consider also the case of strings"
- (= __
+ (= "Hello world, and moon, and stars"
     (-> "Hello world"
         (str ", and moon")
         (str ", and stars")))
 
  "When a function has no arguments to partially apply, just reference it"
- (= __
+ (= "String with a trailing space"
     (-> "String with a trailing space "
         clojure.string/trim))
 
  "Most operations that take a scalar value as an argument can be threaded-first"
- (= __
+ (= 6
     (-> {}
-        (assoc :a 1)
-        (assoc :b 2)
+        (assoc :a 1)             ;; {:a 1}
+        (assoc :b 2)             ;; {:a 1, :b 2}
         (assoc :c {:d 4
-                   :e 5})
-        (update-in [:c :e] inc)
+                   :e 5})        ;; {:a 1, :b 2, :c {:d 4, :e 5}}
+        (update-in [:c :e] inc)  ;; {:a 1, :b 2, :c {:d 4, :e 6}}
         (get-in [:c :e])))
 
  "We can use functions we have written ourselves that follow this pattern"
- (= __
+ (= 1
     (-> {}
         (assoc :a 1)
         (function-that-takes-a-map "hello" "there")))
 
  "We can also thread last using ->>"
- (= __
+ (= [2 3 4]
     (->> [1 2 3]
          (map inc)))
 
  "Most operations that take a collection can be threaded-last"
- (= __
-    (->> a-list
-         (map inc)
-         (filter even?)
-         (into [])
+ (= 12
+    (->> a-list          ;; (1 2 3 4 5)
+         (map inc)       ;; (2 3 4 5 6)
+         (filter even?)  ;; (2 4 6)
+         (into [])       ;; [2 4 6]
          (reduce +)))
 
  "We can use functions we have written ourselves that follow this pattern"
- (= __
-    (->> a-list-with-maps
-         (function-that-takes-a-coll "hello" "there")
+ (= [1 2 3]
+    (->> a-list-with-maps  ;; '({:a 1} {:a 2} {:a 3})
+         (function-that-takes-a-coll "hello" "there")  ;; (1 2 3)
          (into []))))
